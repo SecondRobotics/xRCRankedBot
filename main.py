d424da56f6b7f53b8208d68b846f631fd5370f45
Nicholas Bottone <nick@bottone.io>
2023-04-07 02:19:43 -0400

    Add type safety (#11)

--- main.py
+++ main.py
@@ -10,12 +10,16 @@ from discord.utils import get
 logger = logging.getLogger('discord')
 load_dotenv()
 
-if not os.getenv('DISCORD_BOT_TOKEN'):
+DISCORD_BOT_TOKEN = os.getenv('DISCORD_BOT_TOKEN')
+if not DISCORD_BOT_TOKEN:
     logger.fatal('DISCORD_BOT_TOKEN not found')
     raise RuntimeError('DISCORD_BOT_TOKEN not found')
-if not os.getenv('SRC_API_TOKEN'):
+
+SRC_API_TOKEN = os.getenv('SRC_API_TOKEN')
+if not SRC_API_TOKEN:
     logger.fatal('SRC_API_TOKEN not found')
     raise RuntimeError('SRC_API_TOKEN not found')
+
 
 intents = discord.Intents.all()
 intents.message_content = True
@@ -40,14 +44,21 @@ class RankedBot(commands.Bot):
         # Purge any old messages
 
         qstatus_channel = get(bot.get_all_channels(), id=1009630461393379438)
+        if qstatus_channel is None or not isinstance(qstatus_channel, discord.TextChannel):
+            logger.fatal("Could not find queue status channel")
+            raise RuntimeError("Could not find queue status channel")
         limit = 0
         async for msg in qstatus_channel.history(limit=None):
             limit += 1
         await qstatus_channel.purge(limit=limit)
-        embed = discord.Embed(title="xRC Sim Ranked Queues", description="Ranked queues are open!", color=0x00ff00)
-        embed.set_thumbnail(url="https://secondrobotics.org/logos/xRC%20Logo.png")
-        embed.add_field(name="No current queues", value="Queue to get a match started!", inline=False)
+        embed = discord.Embed(title="xRC Sim Ranked Queues",
+                              description="Ranked queues are open!", color=0x00ff00)
+        embed.set_thumbnail(
+            url="https://secondrobotics.org/logos/xRC%20Logo.png")
+        embed.add_field(name="No current queues",
+                        value="Queue to get a match started!", inline=False)
         await qstatus_channel.send(embed=embed)
+
 
 bot = RankedBot()
 #tree = app_commands.CommandTree(bot)
@@ -55,7 +66,6 @@ bot = RankedBot()
 
 @app_commands.command()
 async def ping(interaction: discord.Interaction):
-    """Pingggg"""
     await interaction.response.send_message('Pong! {0} :ping_pong: '.format(round(bot.latency * 1000, 4)),
                                             ephemeral=False)
 
@@ -75,4 +85,4 @@ stdout_log_handler.setFormatter(logging.Formatter(
 logger.setLevel(logging.INFO)
 logger.addHandler(stdout_log_handler)
 
-bot.run(os.getenv("DISCORD_BOT_TOKEN"), log_handler=None)
+bot.run(DISCORD_BOT_TOKEN, log_handler=None)
