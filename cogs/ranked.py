260bc5b419db933ad88f5d56fece3babb5de7f5d
adam10836 <67599608+adam10836@users.noreply.github.com>
2025-06-10 00:45:48 -0500

    Attempt #2 at updating the submit cooldown. I removed an accident I made.

--- cogs/ranked.py
+++ cogs/ranked.py
@@ -1585,7 +1585,7 @@ class Ranked(commands.Cog):
 
     
     @app_commands.command(description="Submit Score")
-    @app_commands.checks.cooldown(1, 20.0, key=lambda i: i.guild_id)
+    @app_commands.checks.cooldown(1, 120.0, key=lambda i: (i.guild_id,i.current_match.id))
     async def submit(self, interaction: discord.Interaction, red_score: int, blue_score: int):
         logger.info(f"{interaction.user.name} called /submit")
         await interaction.response.defer()
